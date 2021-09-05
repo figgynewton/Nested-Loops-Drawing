b1db0cfa047f15e068f8862551dd331a563f89bb
Newt <72360212+figgynewton@users.noreply.github.com>
2021-09-05 17:39:00 -0400

    Add files via upload

---
+++ NewtonHannahM02_Ch4Ex14.py
@@ -0,0 +1,17 @@
+Python 3.8.5 (v3.8.5:580fbb018f, Jul 20 2020, 12:11:27) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> range (7, 0, -1)
+range(7, 0, -1)
+>>> for x in range(7, 0, -1):
+	print("*" * x)
+
+	
+*******
+******
+*****
+****
+***
+**
+*
+>>> 
